fa0f155074ead1b91cceb74971a8491c2c917122
Mehmet Öner Yalçın <mehmet.yalcin@sky.uk>
2020-03-29 21:34:42 +0100

    More refactoring and adding comments

--- kentikproject/project.py
+++ kentikproject/project.py
@@ -11,23 +11,27 @@ def main():
     root = pathlib.Path('/tmp/data/')
 
     # identify all files in user directiory
-    file_generator =  (file_ for file_ in root.rglob('*') if file_.is_file())
+    file_generator =  (file for file in root.rglob('*') if file.is_file())
 
-    # Initilaize Finite State Machine and fsm
+    # Initilaize Finite State Machine for directory structure
     kentik_fsm = KentikDirectoryFormatFsm()
 
-    for file_ in file_generator:
-        # print(file_)
-        fsm_results = kentik_fsm.parse(str(file_))
+    # Process all files and FSM will filter files only matching the defined FSM
+    for file in file_generator:
+        fsm_results = kentik_fsm.parse(str(file))
 
-
+    # FSM does the initial verification on directory structure,
+    # However verify_dir_tstamp will do a second verification to ensure
+    # directory format produces correct Year/Month/Day/Hour/Minute format
+    # it also returns back tstamp metadata and client_id for next step
     verified_files = verify_dir_tstamp(fsm_header=kentik_fsm.header,
                                        fsm_results=fsm_results)
 
+     # identify files marked for deletion (Don't delete yet), considering
+     # custom retention policy per client
     expired = find_expired(verified_files)
 
-    print("-------------------------------\n")
-    print(expired)
+    return expired
 
 if __name__ == "__main__":
-    main()
+    print(main())
